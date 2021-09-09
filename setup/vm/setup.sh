0af3e7c63a1613e29a111ab11cb353e180ef7af0
ChocolateOverflow <57666296+ChocolateOverflow@users.noreply.github.com>
2021-09-09 13:04:08 +0700

    added stuff for virtualbox shared folder

--- setup/vm/setup.sh
+++ setup/vm/setup.sh
@@ -3,9 +3,12 @@
 set -e
 source ../../var.sh
 
-systemd-detect-virt -q && exit
 echo "[START] Setting up VMs" | tee -a "$log_file"
-sudo modprobe vboxdrv
+if [ systemd-detect-virt ]; then
+  sudo modprobe vboxdrv
+else
+  sudo usermod -aG vboxsf $USER
+fi
 
 #sudo cp modprobe.conf /etc/modprobe.d/
 echo "[DONE] Setting up VMs" | tee -a "$log_file"
