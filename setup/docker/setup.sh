5899d18081e813e2502025a6d88b6677670914eb
ChocolateOverflow <57666296+ChocolateOverflow@users.noreply.github.com>
2022-02-11 20:38:20 +0700

    docker setup no longer adds group

--- setup/docker/setup.sh
+++ setup/docker/setup.sh
@@ -4,7 +4,6 @@ source ../../var.sh
 
 echo "[START] Setting up docker" | tee -a "$log_file"
 sudo gpasswd -a $USER docker
-sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
 echo "[DONE] Setting up docker" | tee -a "$log_file"
