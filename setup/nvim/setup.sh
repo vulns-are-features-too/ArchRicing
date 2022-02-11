d81d42fac8fd38ca777cc71b3ff385192b29a4e3
ChocolateOverflow <57666296+ChocolateOverflow@users.noreply.github.com>
2022-02-11 18:23:16 +0700

    Removed explicit "vim-runtime" install

--- setup/nvim/setup.sh
+++ setup/nvim/setup.sh
@@ -12,9 +12,6 @@ chmod +x ~/.local/bin/nvim
 
 mkdir -p $path_to_vim/autoload $path_to_vim/plugged
 
-# Install stuff with package manager
-sudo pacman -S --needed --noconfirm vim-runtime
-
 # Install vim-plug
 curl "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim" > $path_to_vim/autoload/plug.vim
 
