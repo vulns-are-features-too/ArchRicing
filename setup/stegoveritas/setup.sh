9cc104a3d7a5f2b67a820130fa22fe236bb7744e
ChocolateOverflow <57666296+ChocolateOverflow@users.noreply.github.com>
2024-09-02 20:17:18 +0700

    setup: removed stegoveritas

--- setup/stegoveritas/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/sh
-source  ../../var.sh
-
-echo "[START] Setting up stegoveritas" | tee -a "$log_file"
-stegoveritas_install_deps
-echo "[DONE] Setting up stegoveritas" | tee -a "$log_file"
