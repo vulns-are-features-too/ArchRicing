27571395f592f1ed91046bd3c9e61e5e5e3eebe9
ChocolateOverflow <57666296+ChocolateOverflow@users.noreply.github.com>
2024-09-03 19:38:47 +0700

    chore: removed vagrant plugin setup

--- setup/vagrant/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/sh
-
-set -e
-source ../../var.sh
-
-systemd-detect-virt -q && exit
-echo "[START] Setting up vagrant" | tee -a "$log_file"
-vagrant plugin install vagrant-reload
-echo "[DONE] Setting up vagrant" | tee -a "$log_file"
