6584a6429394275259024bc18c07220a22dcc247
ChocolateOverflow <57666296+ChocolateOverflow@users.noreply.github.com>
2024-09-03 19:41:40 +0700

    setup: commented out gh cli setup

--- setup/git/setup.sh
+++ setup/git/setup.sh
@@ -7,10 +7,10 @@ echo "[START] Setting up git" | tee -a "$log_file"
 
 cp ./.gitconfig ~/
 
-# gh
-gh config set git_protocol ssh
-gh config set editor "$EDITOR"
-gh config set pager bat
-gh config set browser "$BROWSER"
+# # gh
+# gh config set git_protocol ssh
+# gh config set editor "$EDITOR"
+# gh config set pager bat
+# gh config set browser "$BROWSER"
 
 echo "[DONE] Setting up git" | tee -a "$log_file"
