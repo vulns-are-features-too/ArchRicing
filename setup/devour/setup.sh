8c8132c71d27f9a806f84f0917bb33f5573b93cf
vulns-are-features-too <57666296+vulns-are-features-too@users.noreply.github.com>
2024-10-19 14:40:09 +0700

    change name to vulns-are-features-too

--- setup/devour/setup.sh
+++ setup/devour/setup.sh
@@ -4,6 +4,6 @@ set -e
 source ../../var.sh
 
 cd "$path_to_tools"
-git clone --depth 1 "https://github.com/ChocolateOverflow/devour"
+git clone --depth 1 "https://github.com/vulns-are-features-too/devour"
 cd devour
 sudo make install
