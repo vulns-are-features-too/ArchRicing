6e4b6aa9faa31a055929de25e7efa1f2d7a6a94b
vulns-are-features-too <57666296+vulns-are-features-too@users.noreply.github.com>
2025-03-16 21:34:35 +0700

    radare2: replace init with -U

--- setup/radare2/setup.sh
+++ setup/radare2/setup.sh
@@ -5,6 +5,6 @@ source ../../var.sh
 
 type r2pm &>/dev/null || exit
 echo "[START] Setting up radare2" | tee -a "$log_file"
-r2pm init
+r2pm -U
 r2pm -i $(tr '\n' ' ' < ./plugins ) || echo "Failed to install plugins"
 echo "[DONE] Setting up radare2" | tee -a "$log_file"
